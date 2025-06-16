8dc7f8ded0f3ad65a57811c9b36fcd5af8a8734e
rai <96561881+r4ai@users.noreply.github.com>
2025-06-16 23:26:30 +0900

    chore: init tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
-import React from "react"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
+import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
+import "./globals.css"
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen"
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// Render the app
+// biome-ignore lint/style/noNonNullAssertion: #root is guaranteed to exist by the HTML template
+const rootElement = document.getElementById("root")!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
